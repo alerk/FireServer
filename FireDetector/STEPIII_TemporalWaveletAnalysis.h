874609595218fccbb4ba21b480f854f74a033e6d
alerk <nxquyen@gmail.com>
2015-05-18 11:14:15 +0700

    Remove duplicated directory

--- FireDetector/STEPIII_TemporalWaveletAnalysis.h
+++
@@ -1,9 +0,0 @@
-#ifndef STEPIII_TEMPORAL_WAVELET_ANALYSIS
-#define STEPIII_TEMPORAL_WAVELET_ANALYSIS
-
-#include "CommonFunctions.h"
-
-void getFlickerPixel(IplImage *frame[FRAME_BUFFER_SIZE],int nrow_image,int ncol_image,IplImage* MaskOfIdxFirePoints, IplImage* setOfIdxFlickerPoints);
-//unsigned char* getFlickerPixel(IplImage *frame[FRAME_BUFFER_SIZE],int nrow_image,int ncol_image);
-
-#endif
